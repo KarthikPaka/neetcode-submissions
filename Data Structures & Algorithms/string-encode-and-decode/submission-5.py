7774de9da308a19504d057ca641e2d789dbf7582
KarthikPaka <karthik97p@gmail.com>
2026-04-22 12:00:27 -0400

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return "\\&."
+        encoded_str = "\\%.".join(strs)
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        if s == "\\&.":
+            return []
+        decoded_str = s.split("\\%.")
+        return decoded_str
